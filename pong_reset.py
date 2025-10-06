ce8d27fb253124be081bd9b6ac109fd1961fc5dd
whitnlan000 <whitnlan000@alpinesd.org>
2025-10-05 21:02:00 -0600

    Update pong_reset.py
    Signed-off-by: whitnlan000 <whitnlan000@alpinesd.org>

--- pong_reset.py
+++ pong_reset.py
@@ -6,13 +6,15 @@ pong_x = 0
 y = 0
 pong_y = 0
 
-open("pong.csv", "w").write("pong_x,pong_y,bot_x,quality")
+open("pong.csv", "w").write("dir,pong_x,pong_y,bot_x,quality,win")
 
-for i in range(3):
-    x = random.randint(1,1000)
-    y = random.randint(1, 100)
+for i in range(3000):
+
+    dir = random.randint(-1, 1)
+    x = random.randint(1,1000) + 100 *dir
+    y = random.randint(1, 100) + 100 *dir
 
     pong_x = random.randint(1, 1000)
     pong_y = random.randint(1, 1000)
 
-    open("pong.csv", "a").write("\n" + str(pong_x) + "," + str(pong_y) + "," + str(x) + "," + str(x-pong_y))
+    open("pong.csv", "a").write("\n" +str(dir)+","+ str(pong_x) + "," + str(pong_y) + "," + str(x) + "," + str(x-pong_y) + "," + str(abs(pong_x - y) > 500))
